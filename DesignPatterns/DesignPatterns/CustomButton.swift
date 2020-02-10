173fbccc08e3d09403c61a8c85a56a048a4c01d1
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-02-10 16:57:57 -0500

    Custom button refactored

--- DesignPatterns/DesignPatterns/CustomButton.swift
+++ DesignPatterns/DesignPatterns/CustomButton.swift
@@ -9,13 +9,25 @@
 import Foundation
 import UIKit
 
-//@IBDesignable
+@IBDesignable
 class CustomButton: UIButton {
 
     @IBInspectable var borderColor: UIColor = .white
     @IBInspectable var borderWidth: CGFloat = 2
     @IBInspectable var cornerRadius: CGFloat = 50
     @IBInspectable var fillColor: UIColor = .red
+
+    fileprivate func setupShadow(_ rect: CGRect) -> CALayer {
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.fillColor = fillColor.cgColor
+        shadowLayer.path = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).cgPath
+
+        shadowLayer.shadowColor = borderColor.cgColor
+        shadowLayer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+        shadowLayer.shadowRadius = cornerRadius
+        shadowLayer.shadowOpacity = 0.9
+        return shadowLayer
+    }
 
     fileprivate func setup(_ rect: CGRect) {
         let path  = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
@@ -25,15 +37,7 @@ class CustomButton: UIButton {
         self.layer.borderWidth = borderWidth
         self.layer.cornerRadius = cornerRadius
         self.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        let shadowLayer = CAShapeLayer()
-        shadowLayer.fillColor = fillColor.cgColor
-        shadowLayer.path = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).cgPath
-
-        shadowLayer.shadowColor = borderColor.cgColor
-        shadowLayer.shadowOffset = CGSize(width: 0.0, height: 6.0)
-        shadowLayer.shadowRadius = cornerRadius
-        shadowLayer.shadowOpacity = 0.9
-        self.layer.shadowPath = shadowLayer.path
+        let shadowLayer = setupShadow(rect)
         layer.insertSublayer(shadowLayer, at: 0)
     }
 
